9fb0f7bc8e06a637230fd0eb50616ecb42541936
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2021-08-29 19:10:14 +0100

    draftsunday7pm

---
+++ src/components/Animales/AnimalCard.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// // this calls the specific data we want to show on all our animal cards
+// // const AnimalCard = ({}) => {
+
+// // // return (
+// // //  <div>
+// // //      <p>test</p>
+// // //  </div>
+// // // )
+// // }
+
+// // name country
+
+// export default AnimalCard
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+// this is the api show animals in Europe by Region
 const baseUrl =
   "https://apiv3.iucnredlist.org/api/v3/species/region/europe/page/0?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee";
 
+// we need multiple base urls to dispaly name, country, sepcies count, threats, conservation measures and habitats
+
+// this gets the animal data fromt he api
 export const getAllAnimals = () => {
   return axios.get(`${baseUrl}`);
 };
