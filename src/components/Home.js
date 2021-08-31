993026d153d46671f1a6d92221e503c10080904c
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2021-08-31 21:46:20 +0100

    tuesdaydraft

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import elephantIcon from "../styles/images/elephant.jpeg";
 
 const Home = () => {
   return (
     <section className="hero is-fullheight is-primary">
       <div className="hero-body">
+        <figure>
+          <figcaption>Listen to the elephant</figcaption>
+          <audio controls src="http://ring-nature.com/tones/elephant.mp3">
+            Your browser does not support the <code>audio</code> element.
+          </audio>
+        </figure>
+        ;
+        <img src={elephantIcon} alt="This is an elephant" />
         <div className="container has-text-centered">
           <p className="title">THE WILD LIST</p>
           <p className="subtitle">A list of endangered wildlife</p>
