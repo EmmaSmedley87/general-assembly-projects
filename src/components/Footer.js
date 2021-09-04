99175d2e0d654abf706c6f117b43c9e385c3fce7
antony <dearalong@tutamail.com>
2021-09-04 11:00:39 +0100

    some styling stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,13 @@ const Footer = () => {
       <footer className="footer is-small">
         <div className="content has-text-centered">
           <button className="button is-warning is-medium is-rounded">
-            Donate
+            <a
+              href="https://www.savetheelephants.org/get-involved/donate-3/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Donate
+            </a>
           </button>
           <hr />
           <p>Emma Smedley and Antony Long</p>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,22 +3,36 @@ import elephantIcon from "../styles/images/elephant.jpeg";
 
 const Home = () => {
   return (
-    <section className="hero is-fullheight is-success">
-      <div className="hero-body">
-        <figure>
-          <figcaption>Listen to the elephant</figcaption>
-          <audio controls src="http://ring-nature.com/tones/elephant.mp3">
-            Your browser does not support the <code>audio</code> element.
-          </audio>
-        </figure>
-        ;
-        <img src={elephantIcon} alt="This is an elephant" />
-        <div className="container has-text-centered">
-          <p className="title">THE ELEPHANT LIST</p>
-          <p className="subtitle">A list of elephants!</p>
+    <>
+      <section className="hero is-fullheight">
+        <div className="hero-body">
+          <img src={elephantIcon} alt="This is an elephant" />
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="hero is-halfheight">
+        <div className="hero-body">
+          <figure className="has-text-centered">
+            <figcaption>Listen to the elephant</figcaption>
+            <audio controls src="http://ring-nature.com/tones/elephant.mp3">
+              Your browser does not support the <code>audio</code> element.
+            </audio>
+          </figure>
+        </div>
+      </section>
+    </>
+
+    // <section className="hero is-fullheight is-light">
+    //   <div className="hero-body">
+    //     <img src={elephantIcon} alt="This is an elephant" />
+    //     <br  />
+    //     <figure>
+    //       <figcaption>Listen to the elephant</figcaption>
+    //       <audio controls src="http://ring-nature.com/tones/elephant.mp3">
+    //         Your browser does not support the <code>audio</code> element.
+    //       </audio>
+    //     </figure>
+    //   </div>
+    // </section>
   );
 };
 
