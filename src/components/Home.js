276d070f79c52e70673ca9853549cdb3b9cf7910
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2021-09-04 11:20:19 +0100

    addedsearchbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,14 @@ const Home = () => {
   return (
     <section className="hero is-fullheight is-success">
       <div className="hero-body">
-        <figure>
+        {/* <figure>
           <figcaption>Listen to the elephant</figcaption>
           <audio controls src="http://ring-nature.com/tones/elephant.mp3">
             Your browser does not support the <code>audio</code> element.
-          </audio>
-        </figure>
+            <img src=""
+          </audio> */}
+        <div>on click</div>
+        {/* </figure> */}
         ;
         <img src={elephantIcon} alt="This is an elephant" />
         <div className="container has-text-centered">
